e8873f7c27e44c954911c3122c171d43ad70ab3d
Fredrik <fredrik.jonson@student.kyh.se>
2022-11-04 16:17:51 +0100

    AccessDenied, Policies
    added  authorization polices and access denied page

---
+++ WebApp/Helpers/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Helpers
+{
+    public static class Roles
+    {
+        public static readonly string Admin = "admin";
+        public static readonly string SecondlineTechnician = "secondlinetechnician";
+        public static readonly string Technician = "technician";
+    }
+}
