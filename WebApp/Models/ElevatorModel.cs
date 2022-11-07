49af2cc3da87db4840ecb893a3de41e61b296b72
Bobby Sonhjos <robert.jodelsohn@student.kyh.se>
2022-11-07 11:46:13 +0100

    Removed  h2 Section Line

--- WebApp/Models/ElevatorModel.cs
+++ WebApp/Models/ElevatorModel.cs
@@ -5,6 +5,8 @@
         public Guid Id { get; set; }
         public string Location { get; set; } = null!;
         public string Status { get; set; } = null!;
+        public string Ongoing { get; set; } = null!;
+        public string Completed { get; set; } = null!;
 
     }
 }
---
+++ WebApp/Pages/Elevator/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Elevator
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
