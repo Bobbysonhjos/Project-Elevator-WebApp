677b532ce8cd5b19b0a3d4cb9cc2c3a01c544a35
Bobby Sonhjos <robert.jodelsohn@student.kyh.se>
2022-11-09 15:52:13 +0100

    Added errands folder

---
+++ WebApp/Services/IElevatorService.cs
@@ -0,0 +1,32 @@
+﻿using WebApp.Models;
+using Moq;
+
+
+namespace WebApp.Services
+{
+    public interface IElevatorService
+    {
+        public Task<IEnumerable<Elevator>> GetAllElevator();
+        public Task<Elevator> GetElevatorById(Guid id);
+    }
+
+    public class MockElevatorService : IElevatorService
+    {
+        public Task<IEnumerable<Elevator>> GetAllElevator()
+        {
+            return Task.FromResult<IEnumerable<Elevator>>(new List<Elevator>());
+        }
+
+        public Task<Elevator> GetElevatorById(Guid id)
+        {
+            return Task.FromResult(new Elevator());
+
+        }
+        
+            
+
+       
+    }
+
+
+}
