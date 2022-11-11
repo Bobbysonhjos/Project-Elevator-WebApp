33cc28199952b29fc08b767200f1194604c84fd9
Bobby Sonhjos <robert.jodelsohn@student.kyh.se>
2022-11-11 14:39:16 +0100

    Added Sort, & Added filter.

---
+++ WebApp/Models/ElevatorWithErrandsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class ElevatorWithErrandsDto
+    {
+        public Guid Id { get; set; }
+        public string Location { get; set; } = null!;
+        public string ElevatorStatus { get; set; } = null!;
+        public DateTime CreatedDateUtc { get; set; }
+        public IList<ErrandDto> Errands { get; set; } = new List<ErrandDto>();
+    }
+}
